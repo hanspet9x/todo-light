b49c69dd581e09022509b5070840294870bcf401
hanspet9x <peterakinlolu1@gmail.com>
2022-02-03 05:03:01 +0100

    todo controller

---
+++ src/controllers/AppController.ts
@@ -0,0 +1,7 @@
+
+import express from 'express';
+
+const AppController = express.Router();
+
+
+export default AppController
---
+++ src/controllers/todo/TodoController.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const TodoController = express.Router();
+
+TodoController.route('/')
+.get((request: Request, response: Response) => {
+
+})
+.post((request: Request, response: Response) => {
+
+})
+.put((request: Request, response: Response) => {
+
+})
+.delete((request: Request, response: Response) => {
+
+});
+export default TodoController;
